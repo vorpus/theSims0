462ab63c6989cd3205f6cf0a470524a1455cbdef
Li <li3zhang@gmail.com>
2016-12-24 19:39:13 -0500

    Implement stepping and play functionality

--- bundle.js
+++ bundle.js
@@ -75,6 +75,26 @@
 	    thisCanvas.draw(ctx);
 	  });
 	
+	  $('.one-step').on('click', () => {
+	    thisCanvas.processConway(ctx, 1);
+	    thisCanvas.draw(ctx);
+	  });
+	
+	  let intervalName;
+	  $('.step-play').on('click', () => {
+	    $('.play-active').toggleClass('hidden');
+	    
+	    if (intervalName) {
+	      clearInterval(intervalName);
+	      intervalName = null;
+	    } else {
+	      intervalName = setInterval(() => {
+	        thisCanvas.processConway(ctx, 1);
+	        thisCanvas.draw(ctx);
+	      }, 500);
+	    }
+	  });
+	
 	})
 
 
@@ -101,10 +121,15 @@
 	    for (let i = 0; i < this.x; i++) {
 	      newGrid[i] = [];
 	      for (let j = 0; j < this.y; j++) {
-	        newGrid[i][j] = new Cell(i, j, 'path');
+	        newGrid[i][j] = new Cell(i, j, 'dead');
 	      }
 	    }
 	    return newGrid;
+	  }
+	
+	  processConway(ctx, steps) {
+	    this.step();
+	    this.draw(ctx);
 	  }
 	
 	  draw(ctx) {
@@ -119,16 +144,74 @@
 	    }
 	  }
 	
-	  neighbors() {
+	  step() {
+	    let cellsToLive = [];
+	    let cellsToDie = [];
 	
+	    for (let i = 0; i < this.x; i++) {
+	      for (let j = 0; j < this.y; j++) {
+	        let neighborCount = this.neighbors(i,j);
+	        let fate = this.myFate(i, j, neighborCount);
+	
+	        if (fate === 'live') {
+	          cellsToLive.push([i,j]);
+	        } else if (fate === 'die') {
+	          cellsToDie.push([i,j]);
+	        }
+	      }
+	    }
+	    this.createLife(cellsToLive);
+	    this.reaperLife(cellsToDie);
 	  }
+	
+	  createLife(cells) {
+	    cells.forEach((cell) => {
+	      this.cells[cell[0]][cell[1]].live();
+	    });
+	  }
+	
+	  reaperLife(cells) {
+	    cells.forEach((cell) => {
+	      this.cells[cell[0]][cell[1]].die();
+	    });
+	  }
+	
+	  neighbors(i, j) {
+	    let liveNeighborCount = 0;
+	    let myNeighbors = this.cells[i][j].neighbors();
+	
+	    myNeighbors.forEach((neighbor) => {
+	      let neighborHealth = this.cells[neighbor[0]][neighbor[1]].status;
+	      if (neighborHealth === 'alive') {
+	        liveNeighborCount++;
+	      }
+	    });
+	    return liveNeighborCount;
+	  }
+	
+	  myFate(i, j, neighborCount) {
+	    if (this.cells[i][j].status === 'alive') {
+	      if (neighborCount < 2) {
+	        return 'die';
+	      } else if (neighborCount > 3) {
+	        return 'die';
+	      }
+	    } else {
+	      if (neighborCount === 3) {
+	        return 'live';
+	      }
+	    }
+	    return null;
+	  }
+	
+	
 	}
 	
 	Canvas.BG_COLOR = "#ffffff";
-	Canvas.DIM_X = 600;
-	Canvas.DIM_Y = 600;
-	Canvas.CELLS_X = 60;
-	Canvas.CELLS_Y = 60;
+	Canvas.DIM_X = 675;
+	Canvas.DIM_Y = 675;
+	Canvas.CELLS_X = 45;
+	Canvas.CELLS_Y = 45;
 	Canvas.CELLXDIM = Canvas.DIM_X/Canvas.CELLS_X;
 	Canvas.CELLYDIM = Canvas.DIM_Y/Canvas.CELLS_Y;
 	
@@ -137,9 +220,8 @@
 
 /***/ },
 /* 2 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ function(module, exports) {
 
-	const Canvas = __webpack_require__(1);
 	
 	class Cell {
 	  constructor(i, j, status) {
@@ -153,31 +235,51 @@
 	  }
 	
 	  toggle() {
-	    if (this.status === 'path') {
-	      this.status = 'wall';
+	    if (this.status === 'dead') {
+	      this.status = 'alive';
 	    } else {
-	      this.status = 'path';
+	      this.status = 'dead';
 	    }
+	  }
+	
+	  live() {
+	    this.status = 'alive';
+	  }
+	
+	  die() {
+	    this.status = 'dead';
 	  }
 	
 	  draw(ctx) {
 	    ctx.fillStyle = '#000000'
-	    if (this.status === 'wall') {
-	      ctx.fillRect(10*this.x, 10*this.y, 10, 10);
+	    const gdSz = 15;
+	    if (this.status === 'alive') {
+	      ctx.fillStyle = '#000000'
+	      ctx.fillRect(gdSz*this.x, gdSz*this.y, gdSz, gdSz);
 	    } else {
-	      ctx.strokeRect(10*this.x, 10*this.y, 10, 10);
+	      ctx.fillStyle = '#ffffff'
+	      ctx.strokeRect(gdSz*this.x, gdSz*this.y, gdSz, gdSz);
 	    }
 	  }
 	
 	  neighbors() {
 	    const neighborActions = [[1,0], [1,1], [0,1], [-1,1], [-1,-1], [1,-1], [-1,0], [0,-1]]
+	    const gdSz = Cell.GRIDSIZE;
 	    let neighborCells = [];
 	    neighborActions.forEach((move) => {
-	
+	      let newX = move[0] + this.x;
+	      let newY = move[1] + this.y;
+	      if (newX < Cell.CELLS_X && newX >= 0 && newY < Cell.CELLS_Y && newY >= 0) {
+	        neighborCells.push([newX, newY]);
+	      }
 	    });
+	    return neighborCells;
 	  }
 	}
 	
+	Cell.GRIDSIZE = 15;
+	Cell.CELLS_X = 45;
+	Cell.CELLS_Y = 45;
 	module.exports = Cell;
 
 
---
+++ cutefixes.md
@@ -0,0 +1,1 @@
+Stepping and then rendering is an issue. Right now it iterates through every cell to determine if it should live or die. This results in in incorrect behavior.
